be5a6c784e95a2f19b293ae280dbdf4b195ed795
brian-nussbaum <bnussbau@bu.edu>
2013-03-02 16:42:13 -0500

    Added "user" and "message" wrapper files

---
+++ Message.m
@@ -0,0 +1,20 @@
+//
+//  Message.m
+//  User Profile
+//
+//  Created by Nussbaum, Brian, Edward on 3/2/13.
+//  Copyright (c) 2013 Takamichi Suzuki. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+@synthesize user;
+@synthesize text;
+@synthesize messageid;
+@synthesize rating;
+@synthesize sentstatus;
+@synthesize timestamp;
+
+
+@end
